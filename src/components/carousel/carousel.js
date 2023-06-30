47f562d73f1054abafd629ac17d488decf86c25f
Geoff George <geoffgeorgethommana@gmail.com>
2023-06-30 16:22:23 +0530

    carousel

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import ArrowForwardRoundedIcon from '@mui/icons-material/ArrowForwardRounded';
+import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
+import PlayCircleFilledWhiteRoundedIcon from '@mui/icons-material/PlayCircleFilledWhiteRounded';
+
 
 const Carousel = () => {
   return (
@@ -8,6 +12,7 @@ const Carousel = () => {
             <div className='img1'>
                 <h3>Sick Leave for Indie Founders?</h3>
                 <p>Indie Stories · Jan 24 · Episode 234</p>
+                <PlayCircleFilledWhiteRoundedIcon/>
             
             </div>
 
@@ -16,6 +21,7 @@ const Carousel = () => {
             <div className='img2'>
                 <h3>When Should You Let People Pay You?</h3>
                 <p>Indie Stories · Jan 24 · Episode 234</p>
+                <PlayCircleFilledWhiteRoundedIcon/>
             
             </div>
         </div>
@@ -24,18 +30,36 @@ const Carousel = () => {
 
                 <h3>Super Pumped (aka The Saturday Edition)</h3>
                 <p>Indie Stories · Jan 24 · Episode 234</p>
+                <PlayCircleFilledWhiteRoundedIcon/>
             
             </div>
         </div>
         <div className='imgContainer'>
             <div className='img4'>
 
-                <h3></h3>
-                <p></p>
+                <h3>Getting Into Public Speaking as a Developer</h3>
+                <p>Indie Stories · Jan 24 · Episode 234</p>
+                <PlayCircleFilledWhiteRoundedIcon/>
                 
             </div>
 
         </div>
+
+        <div className='arrowContainer1'>
+        
+
+            <ArrowForwardRoundedIcon style={{color:"blue"}}/>
+
+        </div>
+
+        <div className='arrowContainer2'>
+        
+            <ArrowBackRoundedIcon style={{color:"blue"}} />
+            
+
+        </div>
+
+        
 
     </div>
   )
